fdd24e76ec68c8c8ea1eed6ab9ed0c2610735c73
Kristal Joanne Dsouza <77977646+kristal19@users.noreply.github.com>
2021-08-19 09:39:42 +0530

    Add files via upload

---
+++ file_decrypt.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <string.h>
+#include "header.h"
+
+void file_decryption(char key[])
+{
+    //open the cipher text file
+    FILE *f1 = fopen("cipher.txt", "r");
+
+    //codition to check for error in opening the file
+    if (f1 == NULL)
+        printf("error in opening the file");
+
+    else
+    {
+        char ch;
+        int keyLen = strlen(key), i, j;
+
+        //to find the length of the message in the file
+        int ciphertext_len = 0;
+        for (int i = 0; (ch = fgetc(f1)) != EOF; i++)
+        {
+            if ((ch >= 'A') && (ch <= 'Z'))
+                ciphertext_len++;
+        }
+        //printf("cipher text length: %d\n", ciphertext_len);
+
+        char newKey[ciphertext_len];
+
+        //generating new key
+        for (i = 0, j = 0; i < ciphertext_len; ++i, ++j)
+        {
+            if (j == keyLen)
+                j = 0;
+
+            newKey[i] = key[j];
+        }
+        newKey[i] = '\0';
+        printf("\nNew Generated Key: %s\n", newKey);
+
+        rewind(f1); //to bring back the ponter to the beginning of the file
+        char c;
+        printf("The decrypted message is: ");
+        for (int i = 0; (ch = fgetc(f1)) != '\n'; i++)
+        {
+            if ((ch >= 'A') && (ch <= 'Z'))
+            {
+                c = (((ch - newKey[i]) + 26) % 26) + 'A';
+                fputc(c, stdout);
+            }
+            else if ((ch == ' ') || (ch == '\t'))
+            {   
+                fputc(ch, stdout);
+            }
+        }
+    }
+    fclose(f1);
+}
